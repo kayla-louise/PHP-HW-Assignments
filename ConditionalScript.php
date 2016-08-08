d659d6fbbafdba60b326baaf759553ac49b8b667
Kayla Louise <kaylacook88@gmail.com>
2016-08-08 11:06:17 -0700

    Ch2HW

--- ConditionalScript.php
+++ ConditionalScript.php
@@ -2,7 +2,7 @@
 "http://www.w3.org/TR/xhtml/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-<title>Interest Array</title>
+<title>Conditional Script</title>
 <meta http-equiv="content-type"
     content="text/html ; charset=iso-8859-1" />
 </head>
